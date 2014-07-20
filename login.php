90e5577cf6b11531b828270e6b82938aeb60d438
zhuqiang92 <18862610769@163.com>
2014-07-20 21:05:43 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+	<form action="mysql_php.php" method="get">
+	<b>学号</b><input type="text" name="sno" />
+    </br>
+	<b>密码</b><input type="text" name="pwd" />
+    </br>	
+	<input type="submit" value="登录" />
+	</form>
+</body>
+</html>
