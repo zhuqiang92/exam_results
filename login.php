d630d0c5ac964212f9cbc2e4aa27535c7f9576b2
zhuqiang92 <18862610769@163.com>
2014-07-22 20:09:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 </head>
 <body>
-	<form action="mysql_php.php" method="get">
+	<form action="grades.php" method="get">
 	<b>学号</b><input type="text" name="sno" />
     </br>
 	<b>密码</b><input type="text" name="pwd" />
